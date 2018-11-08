1c4d475fe779f1ab4c6a5a2a156f0f12f85f0313
Sean Butterworth <sean_butterworth@hotmail.com>
2018-11-08 10:20:00 -0800

    External Ship Cameras now controllable

--- Expanse/Assets/Scripts/ExternalShipViewController.cs
+++ Expanse/Assets/Scripts/ExternalShipViewController.cs
@@ -8,13 +8,18 @@ public class ExternalShipViewController : MonoBehaviour
 {
     public ExternalShipView m_DefaultView = null;
 
+    public Text m_LabelName = null;
+
+    [Tooltip( "A mouse speed scalar for how fast the camera will rotate" )]
+    public float m_RotateSpeed = 3.5f;
+
     public static ExternalShipViewController GetInstance() { return m_Instance; }
 
     public void SetView( ExternalShipView view )
     {
-        if( m_View != view )
+        if ( m_View != view )
         {
-            if( m_View != null )
+            if ( m_View != null )
             {
                 m_View.Select( false );
             }
@@ -26,21 +31,19 @@ public class ExternalShipViewController : MonoBehaviour
 
         RawImage currentRawImage = GetComponent<RawImage>();
 
-        if( currentRawImage != null )
+        if ( currentRawImage != null )
         {
             RawImage newRawImage = view.GetComponent<RawImage>();
 
-            if( newRawImage != null )
+            if ( newRawImage != null )
             {
                 currentRawImage.texture = newRawImage.texture;
             }
         }
 
-        Text label = GetComponentInChildren<Text>();
-
-        if(label != null)
+        if ( m_LabelName != null )
         {
-            label.text = view.GetLabel();
+            m_LabelName.text = view.GetLabel();
         }
     }
 
@@ -52,19 +55,133 @@ public class ExternalShipViewController : MonoBehaviour
     }
 
     // Use this for initialization
-    private void Start ()
+    private void Start()
     {
-	}
-	
-	// Update is called once per frame
-	private void Update ()
+    }
+
+    // Update is called once per frame
+    private void Update()
     {
+        if ( MouseScreenCheck() )
+        {
+            if ( m_DefaultView != null )
+            {
+                SpaceShipExternalCamera camera = m_View.m_ViewCamera;
+
+                if ( camera != null )
+                {
+                    if ( Input.GetMouseButton( 1 ) )
+                    {
+                        float mouseX = Input.GetAxis( "Mouse X" ) * m_RotateSpeed;
+                        float mouseY = Input.GetAxis( "Mouse Y" ) * -m_RotateSpeed;
+
+                        if ( Cursor.lockState != CursorLockMode.Locked )
+                        {
+                            Cursor.lockState = CursorLockMode.Locked;
+
+                            if ( Cursor.visible )
+                            {
+                                Cursor.visible = false;
+                            }
+                        }
+                        else
+                        {
+
+                            // Add the new rotation delta to the current pan value
+                            //camera.Pan = new Vector2( m_MouseBase.y - mouseY, m_MouseBase.x - mouseX );
+                            camera.Pan = new Vector2( mouseY, mouseX );
+
+                            //UpdateFreeView( camera );
+                        }
+
+                        m_MouseBase = new Vector2( mouseX, mouseY );
+                    }
+                    else
+                    {
+                        if ( Cursor.lockState == CursorLockMode.Locked )
+                        {
+                            Cursor.lockState = CursorLockMode.None;
+                        }
+                        if ( Cursor.visible == false )
+                        {
+                            Cursor.visible = true;
+                        }
+                    }
+                }
+            }
+
+        }
         //bool active = GetComponentInParent<ScreenPanel>().Enabled;
 
         //m_ViewCamera.GetComponent<Camera>().enabled = active;
+        /*
+         *
+        if ( MouseScreenCheck() )
+        {
+            // Dragging motions are only active with the right mouse button depressed
+            if ( Input.GetMouseButton( 1 ) )
+            {
+                if ( Cursor.lockState != CursorLockMode.Locked )
+                {
+                    Cursor.lockState = CursorLockMode.Locked;
+                }
+                if ( Cursor.visible )
+                {
+                    Cursor.visible = false;
+                }
+
+                float mouseX = Input.GetAxis( "Mouse X" ) * m_RotateSpeed;
+                float mouseY = Input.GetAxis( "Mouse Y" ) * -m_RotateSpeed;
+
+                if ( m_Target == null )
+                {
+                    UpdateFreeView( mouseX, mouseY );
+                    transform.Rotate( new Vector3( mouseY, mouseX, 0 ) );
+                    float X = transform.rotation.eulerAngles.x;
+                    float Y = transform.rotation.eulerAngles.y;
+                    transform.rotation = Quaternion.Euler( X, Y, 0 );
+                }
+            }
+            else
+            {
+                if ( Cursor.lockState == CursorLockMode.Locked )
+                {
+                    Cursor.lockState = CursorLockMode.None;
+                }
+                if( Cursor.visible == false )
+                {
+                    Cursor.visible = true;
+                }
+            }
+         */
+    }
+
+    private bool MouseScreenCheck()
+    {
+        RectTransform rectTransform = GetComponent<RectTransform>();
+
+        if ( rectTransform != null )
+        {
+            if ( RectTransformUtility.RectangleContainsScreenPoint( rectTransform, Input.mousePosition, null ) )
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private void UpdateFreeView( SpaceShipExternalCamera camera )
+    {
+        float mouseX = Input.GetAxis( "Mouse X" ) * m_RotateSpeed;
+        float mouseY = Input.GetAxis( "Mouse Y" ) * -m_RotateSpeed;
+
+        // Add the new rotation delta to the current pan value
+        camera.Pan += new Vector2( mouseY, mouseX );
     }
 
     private ExternalShipView m_View = null;
     //private SpaceShipExternalCamera m_ViewCamera = null;
     private static ExternalShipViewController m_Instance = null;
+    private Vector2 m_MouseBase = new Vector2();
 }
--- Expanse/Assets/Scripts/SpaceShipExternalCamera.cs
+++ Expanse/Assets/Scripts/SpaceShipExternalCamera.cs
@@ -4,20 +4,82 @@ public class SpaceShipExternalCamera : MonoBehaviour
 {
     public uint m_MaxZoom = 200;
     public Vector2 m_PanRange = new Vector2( 90.0f, 90.0f );
-    public Vector2 m_PanCenter = new Vector2( 0.0f, 0.0f );
+    //public Vector2 m_PanCenter = new Vector2( 0.0f, 0.0f );
 
     public uint Zoom { get; set; }
-    public Vector2 Pan { get; set; }
+
+    public Vector2 Pan
+    {
+        get
+        {
+            return new Vector2( m_Pan.x, m_Pan.y );
+        }
+
+        set
+        {
+            Debug.Log( "Camera (" + name + ") Pan(" + value.ToString() + ")" );
+
+            // Transfer the pan x value
+            m_Pan.x += value.x;
+            m_Pan.x = Mathf.Min( m_Pan.x, m_MaxPanAngles.x );
+            m_Pan.x = Mathf.Max( m_Pan.x, m_MinPanAngles.x );
+
+            // Transfer the pan y value
+            m_Pan.y += value.y;
+            m_Pan.y = Mathf.Min( m_Pan.y, m_MaxPanAngles.y );
+            m_Pan.y = Mathf.Max( m_Pan.y, m_MinPanAngles.y );
+
+            UpdateOrientation( m_Pan.x, m_Pan.y );
+        }
+    }
 
     public void Reset()
     {
+        m_Pan = m_PanBase;
+
+        UpdateOrientation( m_PanBase.x, m_PanBase.y );
     }
 
-    private void Start()
+    private void Awake()
     {
+        // Set the default orientation
+        m_PanBase = transform.localRotation.eulerAngles;
+
+        // Create a Vec3 copy of the pan range (Vec3 <= Vec2)
+        Vector3 panRange = m_PanRange;
+
+        m_MinPanAngles = ( m_PanBase - panRange );
+        m_MaxPanAngles = ( m_PanBase + panRange );
+
+        Debug.Log("Camera (" + name + ") Orientation(" + m_PanBase.ToString() + ") Min(" + m_MinPanAngles.ToString() + ") Max(" + m_MaxPanAngles.ToString() + ")" );
+
+        Reset();
     }
 
     private void Update()
     {
     }
+
+    private void UpdateOrientation(float x, float y)
+    {
+        Debug.Log( "Camera (" + name + ") UpdateOrientation(" + x.ToString() + ", " + y.ToString() + ")" );
+
+        //transform.Rotate( new Vector3( x, y, 0 ) );
+        //float X = transform.rotation.eulerAngles.x;
+        //float Y = transform.rotation.eulerAngles.y;
+        //transform.rotation = Quaternion.Euler( X, Y, 0 );
+        transform.localRotation = Quaternion.Euler( x, y, 0 );
+    }
+
+    // The base (default) local orientation of the camera
+    private Vector3 m_PanBase;
+
+    // The current local orientation of the camera
+    private Vector3 m_Pan = new Vector3();
+
+    // The minimum boundary local angles
+    private Vector3 m_MinPanAngles;
+
+    // The maximum boundary local angles
+    private Vector3 m_MaxPanAngles;
 }

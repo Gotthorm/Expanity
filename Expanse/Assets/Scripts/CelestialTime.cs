9c8f71348ccb64ffb22dfc468b632df0cb8a946f
Sean Butterworth <sean_butterworth@hotmail.com>
2019-01-06 18:04:42 -0800

    Some progress towards completing the time dilation control
    Added Courier font
    Refactored UI to show time and julian date

--- Expanse/Assets/Scripts/CelestialTime.cs
+++ Expanse/Assets/Scripts/CelestialTime.cs
@@ -1,18 +1,20 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class CelestialTime : MonoBehaviour
 {
-    public Text m_ActualText = null;
-    public InputField m_CurrentText = null;
+    public Text m_CurrentTimeText = null;
+    public Text m_VirtualTimeText = null;
+
+    public Text m_CurrentJulianDateText = null;
+    public Text m_VirtualJulianDateText = null;
 
     public double Actual
     {
         get
         {
-            return m_Actual;
+            return m_CurrentJulianDate;
         }
     }
 
@@ -20,7 +22,7 @@ public class CelestialTime : MonoBehaviour
     {
         get
         {
-            return m_Current;
+            return m_VirtualJulianDate;
         }
     }
 
@@ -37,14 +39,15 @@ public class CelestialTime : MonoBehaviour
         m_Synced = false;
         m_Increasing = true;
         m_Decreasing = false;
-        m_Current += m_IncrementUnit;
+        m_VirtualTime = new DateTime( m_VirtualTime.Ticks + m_TimeIncrementUnit );
         m_PressDuration = 0.0f;
+        Debug.Log( m_VirtualTime.ToLongTimeString() );
     }
 
     public void IncreaseCurrentTimeRelease()
     {
         m_Increasing = false;
-        m_CurrentIncrement = m_MinIncrement;
+        m_CurrentTimeIncrement = m_MinCurrentTimeIncrement;
     }
 
     public void DecreaseCurrentTimePress()
@@ -52,14 +55,14 @@ public class CelestialTime : MonoBehaviour
         m_Synced = false;
         m_Decreasing = true;
         m_Increasing = false;
-        m_Current -= m_IncrementUnit;
+        m_VirtualTime = new DateTime( m_VirtualTime.Ticks - m_TimeIncrementUnit );
         m_PressDuration = 0.0f;
     }
 
     public void DecreaseCurrentTimeRelease()
     {
         m_Decreasing = false;
-        m_CurrentIncrement = m_MinIncrement;
+        m_CurrentTimeIncrement = m_MinCurrentTimeIncrement;
     }
 
     public void SyncCurrentClicked()
@@ -70,8 +73,10 @@ public class CelestialTime : MonoBehaviour
     private void Awake()
     {
         m_Instance = this;
-        m_Actual = PlanetPositionUtility.GetJulianDate( System.DateTime.Now );
-        m_Current = m_Actual;
+        m_CurrentTime = DateTime.Now;
+        m_VirtualTime = m_CurrentTime;
+        m_CurrentJulianDate = PlanetPositionUtility.GetJulianDate( m_CurrentTime );
+        m_VirtualJulianDate = m_CurrentJulianDate;
     }
 
     private void Update()
@@ -83,47 +88,72 @@ public class CelestialTime : MonoBehaviour
             // Treat press like a single click until the button is held more than a certain duration
             if( m_PressDuration > 0.5f )
             {
-                m_CurrentIncrement += (uint)(100U * Time.deltaTime);
+                m_CurrentTimeIncrement += (long)(100 * Time.deltaTime);
 
-                if( m_CurrentIncrement > m_MaxIncrement )
+                if ( m_CurrentTimeIncrement > m_MaxCurrentTimeIncrement )
                 {
-                    m_CurrentIncrement = m_MaxIncrement;
+                    m_CurrentTimeIncrement = m_MaxCurrentTimeIncrement;
                 }
 
-                double increment = m_IncrementUnit * ( m_CurrentIncrement * m_CurrentIncrement );
+                long timeIncrement = m_TimeIncrementUnit * ( m_CurrentTimeIncrement * m_CurrentTimeIncrement );
 
-                m_Current += ( m_Increasing ) ? increment : -increment;
+                m_VirtualTime = new DateTime( m_VirtualTime.Ticks + ( (m_Increasing) ? timeIncrement : -timeIncrement ) );
+
+                m_VirtualJulianDate = PlanetPositionUtility.GetJulianDate( m_VirtualTime );
             }
         }
 
-        m_Actual = PlanetPositionUtility.GetJulianDate( System.DateTime.Now );
+        m_CurrentTime = DateTime.Now;
+        m_CurrentJulianDate = PlanetPositionUtility.GetJulianDate( m_CurrentTime );
 
-        if( m_ActualText != null )
+        if( m_CurrentJulianDateText != null )
         {
-            m_ActualText.text = m_Actual.ToString(".00000");
+            m_CurrentJulianDateText.text = m_CurrentJulianDate.ToString(".00000");
+        }
+
+        if( m_CurrentTimeText != null )
+        {
+            m_CurrentTimeText.text = GetTimeString( m_CurrentTime );
         }
 
         if( m_Synced )
         {
-            m_Current = m_Actual;
+            m_VirtualJulianDate = m_CurrentJulianDate;
+            m_VirtualTime = m_CurrentTime;
         }
 
-        if ( m_CurrentText != null )
+        if ( m_VirtualJulianDateText != null )
         {
-            m_CurrentText.text = m_Current.ToString( ".00000" );
+            m_VirtualJulianDateText.text = m_VirtualJulianDate.ToString( ".00000" );
         }
+
+        if( m_VirtualTimeText != null )
+        {
+            m_VirtualTimeText.text = GetTimeString( m_VirtualTime );
+        }
+    }
+
+    private string GetTimeString( DateTime dateTime )
+    {
+        // 2015/12/12 12:34:26 PM
+        string results = string.Format( "{0}/{1}/{2} ", dateTime.Year.ToString(), dateTime.Month.ToString("00"), dateTime.Day.ToString("00") );
+        results += string.Format( "{0}:{1}:{2} PM", dateTime.Hour.ToString("00"), dateTime.Minute.ToString("00"), dateTime.Second.ToString("00") );
+        return results;
     }
 
     private static CelestialTime m_Instance = null;
 
-    private double m_Actual = 0.0;
-    private double m_Current = 0.0;
+    private double m_CurrentJulianDate = 0.0;
+    private double m_VirtualJulianDate = 0.0;
     private bool m_Synced = true;
     private bool m_Increasing = false;
     private bool m_Decreasing = false;
-    private const float m_IncrementUnit = 0.00001f;
     private float m_PressDuration = 0.0f;
-    private const uint m_MinIncrement = 1U;
-    private const uint m_MaxIncrement = 4000U;
-    private uint m_CurrentIncrement = m_MinIncrement;
+
+    private DateTime m_CurrentTime;
+    private DateTime m_VirtualTime;
+    private const long m_MinCurrentTimeIncrement = 1;
+    private const long m_MaxCurrentTimeIncrement = 4000;
+    private const long m_TimeIncrementUnit = 10000000; // One second
+    private long m_CurrentTimeIncrement = m_MinCurrentTimeIncrement;
 }
